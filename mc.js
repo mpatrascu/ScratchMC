e348543c01981842279999df602582cd5178f890
Mircea Patrascu <Mircea@Mac-mini.local>
2016-11-01 23:31:21 +0200

    added new report blocks and new functions
    -drone created at target block position
    -added ‘marker ‘ block - useful to show drone position/orientation
    -sounds can play for one or all players
    -MC time and weather commands
    -turn command sets orientation of player
    -sends to Scratch positions of player and drone

--- mc.js
+++ mc.js
@@ -4,10 +4,10 @@ var spawn = require('child_process').spawn;
 // Create a child process for the Minecraft server using the same java process
 // invocation we used manually before
 var minecraftServerProcess = spawn('java', [
-    '-Xmx1024M',
+    '-Xmx4096M',
     '-Xms1024M',
     '-jar',
-    'minecraft_server_Spigot.jar',
+    'spigot-1.10.jar',
     'nogui'] );
 
 // Listen for events coming from the minecraft server process - in this case,
@@ -26,20 +26,21 @@ scratchResponse=[];  //stores the IP and current block values in MC for each Scr
 http.createServer(function(request, response) { //communicates with Scratch extension on port 8088
 var ip = request.connection.remoteAddress.slice(7);
 
-if (request.url!="/poll"){ //commands from Scratch - URL format is /command/first_param/second_param/../last_param/
+if (request.url != "/poll"){ //commands from Scratch - URL format is /command/first_param/second_param/../last_param/
 	
 	var command=request.url.toString().slice(1);
 	minecraftServerProcess.stdin.write('js scratch("' + command + '","' + ip + '")' + '\n');
 	response.end();
 	
-	if (request.url=="/reset_all"){ //end of Scratch session		
-		position=-1;
+	
+	if (request.url == "/reset_all"){ //end of Scratch session		
+		position = -1;
 		for(i=0;i<scratchResponse.length; i++){
-			if (scratchResponse[i].ip==ip){
-				position=i;				
+			if (scratchResponse[i].ip == ip){
+				position = i;				
 				}
 			}
-		if (position>-1) scratchResponse.splice(position,1);
+		if (position > -1) scratchResponse.splice(position, 1);
 		console.log('Scratch client on IP: ' + ip + ' disconnected. Remaining ' + scratchResponse.length + ' connections.');
 		}	
   }
@@ -47,39 +48,95 @@ else{ //polling by Scratch
 	bltype = -1;
 	bldata = -1;
 	result = -1;
+	drone = {x : -1,
+			 y : -1,
+			 z : -1};
+	player = {x : -1,
+			 y : -1,
+			 z : -1};
 	
 	for(i=0;i<scratchResponse.length; i++){
-		if (scratchResponse[i].ip==ip){
-			bltype=scratchResponse[i].bltype;
-			bldata=scratchResponse[i].bldata;
-			result=scratchResponse[i].result;
+		if (scratchResponse[i].ip == ip){
+			bltype = scratchResponse[i].bltype;
+			bldata = scratchResponse[i].bldata;
+			drone = scratchResponse[i].drone;
+			player = scratchResponse[i].player;
+			result = scratchResponse[i].result;
 		}
 	}
 
-	response.write("blockType " + bltype + "\nblockData "+bldata + "\nresult " + result);
-	response.end();
+	
+	response.write("blockType " + bltype + "\nblockData " +  bldata);
+	response.write("\nposition/x/drone " + drone.x + "\nposition/y/drone " + drone.y + "\nposition/z/drone " + drone.z);
+	response.write("\nposition/x/player " +  player.x + "\nposition/y/player " + player.y + "\nposition/z/player " + player.z);
+	response.write("\nresult " + result);
+	response.end()
 
 	}
 }).listen(8088);
 
-http.createServer(function(request, response) {  //receives updates from Minecraft server on port 8089 - URL format is /blockType/blockData/IP/
-params=request.url.toString().slice(1).split("/");
-console.log("update information received: " + request.url.toString().slice(1));
-ip=params[3];
-
-found=0;
-for (i=0; i < scratchResponse.length;i++){
-    if(scratchResponse[i].ip==ip){
-        scratchResponse[i].bltype=params[0];
-        scratchResponse[i].bldata=params[1];
-        scratchResponse[i].result=params[2];
-        found=1;
-    }
-}
-if (!found)
-    scratchResponse.push({bltype:params[0], bldata: params[1], result: params[2], ip:params[3]});
-
-response.write('ip'+(found?' found':' added to list'));
-response.end();
+http.createServer(function(request, response) {  //receives updates from Minecraft server on port 8089 
+	var	found=0;
+	var params = request.url.toString().slice(1).split("/");
+	console.log("update information received: " + request.url.toString().slice(1));
+	
+	switch(params[0]){
+		case 'init': //received at start and when using /js refresh()
+			while (scratchResponse.length > 0 )
+				scratchResponse.pop();
+			response.write('cleared list of clients');
+			response.end();
+			return;
+			break;
+		case 'drone': 
+			ip = params[7];
+			break;
+		case 'player': 
+			ip = params[4];
+			break;
+		}
+	
+	for (i = 0; i < scratchResponse.length; i++){
+		if(scratchResponse[i].ip == ip){
+			found = 1;
+			switch(params[0]){
+				case 'drone': 
+					scratchResponse[i].bltype = params[1];
+					scratchResponse[i].bldata = params[2];
+					scratchResponse[i].drone.x = params[3];
+					scratchResponse[i].drone.y = params[4];
+					scratchResponse[i].drone.z = params[5];
+					scratchResponse[i].result = params[6];
+					break;
+				case 'player':
+					scratchResponse[i].player.x = params[1];
+					scratchResponse[i].player.y = params[2];
+					scratchResponse[i].player.z = params[3];
+					break;		
+			}
+		}
+	}
+	if (!found){
+		switch(params[0]){
+				case 'drone': 
+					scratchResponse.push({bltype: params[1],
+										bldata: params[2],
+										drone: {x: params[3], y: params[4], z: params[5]},
+										player: {x: null, y: null, z: null},
+										result: params[6],
+										ip: params[7]});
+					break;
+				case 'player':
+					scratchResponse.push({bltype:-1,
+										bldata: -1,
+										drone: {x: null, y: null, z: null},
+										player: {x: params[1], y: params[2], z: params[3]},
+										result: params[6],
+										ip: params[7]});
+					break;
+		}
+	}
+	response.write('IP ' + ip + (found ? ' found' : ' added to list'));
+	response.end();
 
 }).listen(8089);
--- scratch.js
+++ scratch.js
@@ -8,15 +8,18 @@ var sounds = require('sounds');
 var teleport = require('teleport');
 
 var scratchReturnAddress = 'http://127.0.0.1:8089/'; 
+
+http.request( scratchReturnAddress + 'init', function(responseCode, responseBody){
+	console.log( responseBody );});
     
 scratchClients=[];
 
 
-function scratch(command,ip){
+function scratch(command, ip){
 	var players = utils.players();	
     var cmd = command.split("/");
     var index = -1;	
-	var drona = '';
+	var drona ;
 
 	for(i = 0; i < scratchClients.length; i++){
 		if (scratchClients[i].ip == ip){
@@ -27,16 +30,24 @@ function scratch(command,ip){
 			}
 		}
 	
-	if(cmd[0] == 'connect'){
+	if(cmd[0] == 'connect'){ //resets drone?
     		console.log('trying to connect to player ' + cmd[1]);
     		playerName = cmd[1];
     		targetPlayer = server.getPlayer(playerName);
     		if(targetPlayer != null){
-    			drona = new Drone (targetPlayer.location);
+    			if (utils.getMousePos(targetPlayer)){
+    				droneLocation = utils.getMousePos(targetPlayer); //takes the position of the block the player is looking at
+    				droneLocation.setYaw(targetPlayer.location.getYaw()); //keeps the orientation of the player
+    				drona = new Drone (droneLocation);
+    				}
+    			else{
+    				drona = new Drone (targetPlayer.location);
+    				}
     			if (index == -1){ //new player
-    				scratchClients.push({playerName:playerName, targetPlayer:targetPlayer,drone:drona, ip:ip});
-    				index = scratchClients.length-1;
-    				echo(targetPlayer, "Connected to Scratch client on IP "+ip);
+    				scratchClients.push({playerName: playerName, targetPlayer: targetPlayer, drone: drona, ip: ip});
+    				scratchUpdatePlayer(targetPlayer, ip);
+    				index = scratchClients.length - 1;
+    				targetPlayer.sendTitle("Scratch","connected to " + ip);
     				}
     			else { //update existing player
     				scratchClients[index].playerName = playerName;
@@ -47,22 +58,25 @@ function scratch(command,ip){
     			   
     		if (index == -1) { //use of other command before connecting to any player
 				console.log('Player not found');
-				scratchReturn(-1, -1, 'player_not_found', ip); 
-				return
+				scratchReturn(null, 'player_not_found', ip); 
+				return;
 				}	
 			else { //return to Scratch current block
 				bl = drona.getBlock();
-				scratchReturn(bl.typeId, bl.data, 'connected', ip); //updates values for current block to be read by Scratch 	
+				scratchReturn(bl, 'connected', ip); //updates values for current block to be read by Scratch 	
 				}
     		}    			
 		
 	if (index == -1) { //use of other command before connecting to any player
 		console.log('No target player defined');
-		scratchReturn(-1, -1, 'no_target_player', ip); 
-		return
+		scratchReturn(null, 'no_target_player', ip); 
+		return;
 		}
 	console.log("command received -" + command+";");
 	switch(cmd[0]){   
+		case 'reset_all': //Scratch client disconnected
+			scratchClients.splice(index, 1);
+			break;
 	    case 'build'://for building commands: box, box0, cylinder, prism, etc
 	    	drona[cmd[1]]('' + cmd[2] + ':' + parseInt(cmd[3]), parseInt(cmd[4]), parseInt(cmd[5]), parseInt(cmd[6])); 
 	    	break;
@@ -94,8 +108,27 @@ function scratch(command,ip){
 						textArray.push(decodeURIComponent(cmd[i]));
 					drona[cmd[1]](textArray);  			
 					break;	
+				case 'marker':
+					switch(drona.dir){
+						case 0: //east
+							skullDir = 5; //?
+							break;
+						case 1: //south
+							skullDir = 3;
+							break;
+						case 2: //west
+							skullDir = 4; //?
+							break;
+						case 3: //north
+							skullDir = 2;
+							break;	
+						}
+					drona.box("144:" + skullDir);
+					console.log(drona.dir);
+					break;
 				}
     		break;	
+    		
     		
 		case 'summon':   //summons mobs using Scriptcraft predefined drone's summon method
 			drona.spawn(cmd[1]) ;   		
@@ -103,45 +136,123 @@ function scratch(command,ip){
 			
 		case 'entitySound':   //plays the sounds of the specified entity on each connected player MC session
 			utils.foreach( players, function( player ) { 
-				sounds['entity' + cmd[1] + cmd[2]]( player ); // spigot 1.10
+				if (cmd[3].toLowerCase() == 'all' || cmd[3].toLowerCase() == player.name.toLowerCase())
+					sounds['entity' + cmd[1] + cmd[2]]( player ); 
 			} ); 		
 			break ;
 			
   		case 'blockSound':   //plays the sounds of the specified block on each connected player MC session
 			utils.foreach( players, function( player ) { 
-				sounds['block' + cmd[1] + cmd[2]]( player ); // spigot 1.10
+				if (cmd[3].toLowerCase() == 'all' || cmd[3].toLowerCase() == player.name.toLowerCase())
+					sounds['block' + cmd[1] + cmd[2]]( player ); 
 			} ); 		
 			break ;
 			
 		case 'music': //plays the specified record on each connected player MC session
 			utils.foreach( players, function( player ) { 
-				sounds['record' + cmd[1]]( player ); // spigot 1.10
+				if (cmd[3].toLowerCase() == 'all' || cmd[3].toLowerCase() == player.name.toLowerCase())
+					sounds['record' + cmd[1]]( player ); 
 			} ); 		
 			break ;
 			    
+		case 'time':
+			switch(cmd[1]){
+				case 'day':
+					drona.world.setTime(1000);
+					break;
+				case 'night':
+					drona.world.setTime(14000);
+					break;
+				default:	
+					drona.world.setTime(cmd[1]);
+					break;
+				}
+			break;
+			
+		case 'weather':
+			switch(cmd[1]){
+				case 'clear':
+					drona.world.setStorm(false);
+					drona.world.setThundering(false);
+					break;
+				case 'rain':
+					drona.world.setStorm(true);
+					drona.world.setThundering(false);
+					break;
+				case 'thunders':
+					drona.world.setStorm(true);
+					drona.world.setThundering(true);
+					break;
+			}
+			break;
+		
 		case 'teleport': //teleports specified player to new coordinates
 			utils.foreach( players, function( player ) { 
 				if (player.name.toLowerCase() == cmd[1].toLowerCase()){
 					newlocation = player.location;
 					if(cmd[2] == 'to'){ //absolute coordinates
-						newlocation.x = parseInt(cmd[3]);
-						newlocation.y = parseInt(cmd[4]);
-						newlocation.z = parseInt(cmd[5]);
+						newlocation.x = parseFloat(cmd[3]);
+						newlocation.y = parseFloat(cmd[4]);
+						newlocation.z = parseFloat(cmd[5]);
 					}
 					else{ //relative coordinates
-						newlocation.x += parseInt(cmd[3]);
-						newlocation.y += parseInt(cmd[4]);
-						newlocation.z += parseInt(cmd[5]);
+						newlocation.x += parseFloat(cmd[3]);
+						newlocation.y += parseFloat(cmd[4]);
+						newlocation.z += parseFloat(cmd[5]);
 					}
 					teleport (player, newlocation);
 				}
 			} );
 			break;
-			    
+			
+		case 'turn': //set yaw parameter for player (direction where it looks)
+			 utils.foreach( players, function( player ) { 
+				if (player.name.toLowerCase() == cmd[1].toLowerCase()){
+					newlocation = player.location;
+					console.log(player.name);
+					console.log("initial yaw: " + newlocation.getYaw());
+					
+					switch(cmd[2]){
+						case 'to_direction': //direction set to cmd[3] value
+							newlocation.setYaw(parseInt(cmd[3]));
+							break
+						case 'by': //player will turn by cmd[3] degrees
+							newlocation.setYaw(newlocation.getYaw() + parseInt(cmd[3]));
+							break;
+						case 'to_point': //player will look at specified point
+							dx = newlocation.x - parseFloat(cmd[3]);
+							dy = newlocation.y + 1 - parseFloat(cmd[4]);
+							dz = newlocation.z - parseFloat(cmd[5]);
+							dh = Math.sqrt(dx * dx + dz * dz);
+							newlocation.setYaw(Math.atan2(-dx, dz) * 180 / Math.PI - 180);
+							newlocation.setPitch(- Math.atan2(-dy, dh) * 180 / Math.PI - 180);
+							break;
+						case 'to_drone': //player will look at the drone position
+							dx = newlocation.x - drona.getBlock().getX() - 0.5;
+							dy = newlocation.y + 1 - drona.getBlock().getY();
+							dz = newlocation.z - drona.getBlock().getZ() - 0.5;
+							dh = Math.sqrt(dx * dx + dz * dz);
+							newlocation.setYaw(Math.atan2(-dx, dz) * 180 / Math.PI - 180);
+							newlocation.setPitch(- Math.atan2(-dy, dh) * 180 / Math.PI);
+							break;					
+						}
+					console.log("final yaw: " + newlocation.getYaw());
+					teleport (player, newlocation);
+				}
+			} );
+			break;   
+			
     	case 'moveDrone':
 			switch(cmd[1]){	
 				case 'reset':
-    				drona = new Drone (targetPlayer.location);
+					if (utils.getMousePos(targetPlayer)){
+						droneLocation = utils.getMousePos(targetPlayer); //takes the position of the block the player is looking at
+						droneLocation.setYaw(targetPlayer.location.getYaw()); //keeps the orientation of the player
+						drona = new Drone (droneLocation);
+						}
+					else{
+						drona = new Drone (targetPlayer.location);
+						}
     				scratchClients[index].drone = drona;   			
     				break;
 				case 'up':
@@ -159,17 +270,40 @@ function scratch(command,ip){
 					drona.move(cmd[2]); 
 					break;
     			}		
-    		bl = drona.getBlock();
-		scratchReturn(bl.typeId, bl.data, 'ok', ip); //updates values for current block to be read by Scratch
-		break;
+			currentBlock = drona.getBlock();
+			scratchReturn(currentBlock, 'ok', ip); //updates values for current block to be read by Scratch
+			break;
+
     }   
 };
 
 
 
-function scratchReturn(bltype, bldata, result, ip){ //updates values for current block to be read by Scratch
-	var http = require('http');
-	http.request( scratchReturnAddress + bltype + '/' + bldata + '/' + result + '/' +ip, function(responseCode, responseBody){console.log(  responseBody );});
+function scratchReturn(block, result, ip){ //updates values for current block to be read by Scratch
+
+	if (block != null)
+		http.request( scratchReturnAddress + 'drone/' + block.typeId + '/' + block.data + '/' + block.getX() + '/' + block.getY() + '/' + block.getZ() + '/' + result + '/' +ip, function(responseCode, responseBody){
+			console.log( responseBody );});
+	else
+		http.request( scratchReturnAddress + 'drone/-1/-1/-1/-1/-1/' + result + '/' +ip, function(responseCode, responseBody){
+			console.log( responseBody );});
 }
 
-exports.scratch = scratch
+function scratchUpdatePlayer(player, ip){ //updates values for current block to be read by Scratch
+
+	http.request( scratchReturnAddress + 'player/' + Math.floor(player.location.x) + '/' + Math.floor(player.location.y) + '/' + Math.floor(player.location.z) + '/' +ip, function(responseCode, responseBody){
+		console.log( responseBody );});
+
+}
+
+function playerMoveScratch(event){
+	for(i = 0; i < scratchClients.length; i++){
+		if (scratchClients[i].targetPlayer == event.player){
+			scratchUpdatePlayer(event.player, scratchClients[i].ip);
+			}
+		}
+
+}
+
+exports.scratch = scratch;
+events.playerMove(playerMoveScratch);
